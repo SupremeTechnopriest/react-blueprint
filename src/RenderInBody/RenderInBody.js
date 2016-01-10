ae55bdf2073fa0f514e214bd70e890764868581f
supremetechnopriest <randylebeau@gmail.com>
2016-01-09 19:14:50 -0500

    added RenderInBody decorator, made Radium a peerDependency

---
+++ src/RenderInBody/RenderInBody.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+export const RenderInBody = (ComposedComponent, options = {}) => {
+
+	return class extends Component {
+
+		child = null;
+
+		componentDidMount() {
+			this.child = document.createElement("div");
+			document.body.appendChild(this.child);
+			this._renderLayer();
+		}
+
+		componentDidUpdate() {
+			this._renderLayer();
+		}
+
+		componentWillUnmount() {
+			ReactDOM.unmountComponentAtNode(this.child);
+			document.body.removeChild(this.child);
+		}
+
+		_renderLayer() {
+			ReactDOM.render(<ComposedComponent />, this.child);
+		}
+
+		render() {
+			// Render a placeholder
+			return React.DOM.div();
+		}
+
+	}
+
+}
+
+export default RenderInBody;
