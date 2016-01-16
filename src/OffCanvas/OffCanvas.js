ef5ff34e37d2b230b79fdd2b6fe727f371574030
supremetechnopriest <randylebeau@gmail.com>
2016-01-16 01:28:25 -0500

    fix off canvas

--- src/OffCanvas/OffCanvas.js
+++ src/OffCanvas/OffCanvas.js
@@ -45,7 +45,11 @@ export default class OffCanvas extends Component {
 		partial: true
 	};
 
-	state = {};
+	state = {
+		openRight: false,
+		openLeft: false,
+		expanded: false
+	};
 
 	constructor(props) {
 		super(props);
@@ -63,44 +67,40 @@ export default class OffCanvas extends Component {
 		let mainStyles = (paddingLeft, paddingRight) => [ styles.content, { paddingLeft, paddingRight }, style];
 		let auxbarStyles = width => [ styles.auxbar, { width }, rightStyle ];
 
+		let defaults = {
+			leftWidth: this._calculateLeft('width'),
+			rightWidth: this._calculateRight('width'),
+			paddingLeft: this._calculateLeft('padding'),
+			paddingRight: this._calculateRight('padding')
+		};
+
+		let springs = {
+			leftWidth: spring(this._calculateLeft('width')),
+			rightWidth: spring(this._calculateRight('width')),
+			paddingLeft: spring(this._calculateLeft('padding')),
+			paddingRight: spring(this._calculateRight('padding'))
+		};
+
 		return (
 			<div style={styles.container}>
-				
-				<Motion style={{ width: spring(this._calculateLeft('width')) }}>
-					{({ width }) => {
+							
+				<Motion defaultStyle={defaults} style={springs}>
+					{({ leftWidth, rightWidth, paddingLeft, paddingRight }) => {
 						return (
-							<div style={sidebarStyles(width)}>
-								{this.props.leftSidebar}
+							<div>
+								<div style={sidebarStyles(leftWidth)}>
+									{this.props.leftSidebar}
+								</div>
+								<div style={mainStyles(paddingLeft, paddingRight)}>
+									{this.props.children}
+								</div>
+								<div style={auxbarStyles(rightWidth)}>
+									{this.props.rightSidebar}
+								</div>
 							</div>
 						);
 					}}
 				</Motion>
-				
-				<Motion 
-					style={{ 
-						paddingLeft: spring(this._calculateLeft('padding')),
-						paddingRight: spring(this._calculateRight('padding'))
-					}}>
-					{({ paddingLeft, paddingRight }) => {
-						return (
-							<div style={mainStyles(paddingLeft, paddingRight)}>
-								{this.props.children}
-							</div>
-						);
-					}}
-
-				</Motion>
-
-				<Motion style={{ width: spring(this._calculateRight('width')) }}>
-					{({ width }) => {
-						return (
-							<div style={auxbarStyles(width)}>
-								{this.props.rightSidebar}
-							</div>
-						)
-					}}
-				</Motion>
-
 			</div>
 		);
 
