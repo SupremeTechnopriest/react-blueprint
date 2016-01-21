d03c6922d55462283de3ec73c118f92e21169537
yrokhlin <yrokhlin@gmail.com>
2016-01-21 17:39:29 -0500

    Refactored so that RenderInBody can be used as a decorator or as JSX

--- src/RenderInBody/RenderInBody.js
+++ src/RenderInBody/RenderInBody.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+class RenderInBody extends Component {
+	
+		componentDidMount() {
+			this.child = document.createElement("div");
+			document.body.appendChild(this.child);
+			this._renderLayer();
+		}
 
-export const RenderInBody = (ComposedComponent, options = {}) => {
+		componentDidUpdate() {
+			this._renderLayer();
+		}
+
+		componentWillUnmount() {
+			React.unmountComponentAtNode(this.child);
+			document.body.removeChild(this.child);
+		}
+
+
+		_renderLayer() {
+			ReactDOM.render(this.props.children, this.child);
+		}
+
+		render() {
+			// Render a placeholder
+			return React.DOM.div();
+		}
+		
+
+}
+
+const decorateRenderInBody = (ComposedComponent, options = {}) => {
 
 	return class extends Component {
 
@@ -36,4 +65,15 @@ export const RenderInBody = (ComposedComponent, options = {}) => {
 
 }
 
-export default RenderInBody;
+const _RenderInBody = (element, options = {}) => {
+	switch(typeof element) {
+		case 'object':
+			return (<RenderInBody>{element.children}</RenderInBody>);
+		case 'function':
+			return decorateRenderInBody(element);
+		default:
+			return;
+	}
+}
+
+export default _RenderInBody;
